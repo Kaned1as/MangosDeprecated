3a82c9a201b70e48d44dc0ebf8e799fede076756
Адонай <adonai@xaker.ru>
2009-12-12 03:45:08 +0000

    - верный расчёт масштаба добавления энергии - следим за стабильностью
    git-svn-id: svn://300murlocs.com/core/stable/code@405 b8eaa9ba-51a4-de11-8768-001a646d8106

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -10790,6 +10790,14 @@ void Unit::SetPower(Powers power, uint32 val)
 void Unit::SetMaxPower(Powers power, uint32 val)
 {
     uint32 cur_power = GetPower(power);
+    uint32 cur_max_power = GetMaxPower(power);
+
+    if(cur_max_power <= 0)
+        return;
+
+    float coef = (float)val / cur_max_power;     // < 1 - power decrease
+    uint32 cur_val = (uint32)(coef * cur_power); // > 1 - power increase
+
     SetStatInt32Value(UNIT_FIELD_MAXPOWER1 + power, val);
 
     // group update
@@ -10809,8 +10817,9 @@ void Unit::SetMaxPower(Powers power, uint32 val)
         }
     }
 
-    if(val < cur_power)
-        SetPower(power, val);
+    
+    //if(val < cur_power)
+        SetPower(power, cur_val);
 }
 
 void Unit::ApplyPowerMod(Powers power, uint32 val, bool apply)
