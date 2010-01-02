acbfa06c38ee5df7531a093fc9128b652cc481ca
Ranger <Ranger@b8eaa9ba-51a4-de11-8768-001a646d8106>
2010-01-02 10:30:06 +0000

    fix http://www.wowhead.com/?item=21213
    git-svn-id: svn://300murlocs.com/core/stable/code@485 b8eaa9ba-51a4-de11-8768-001a646d8106

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2257,6 +2257,11 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                 if(m_target->GetTypeId() == TYPEID_PLAYER)
                     ((Player*)m_target)->RemoveAmmo();      // not use ammo and not allow use
                 return;
+            case 62061:                             // Festive Holiday Mount
+                if (m_target->HasAuraType(SPELL_AURA_MOUNTED))
+                    // Reindeer Transformation
+                    m_target->CastSpell(m_target, 25860, true, NULL, this);
+                return;
             //warlock: haunt
             case 48181:
             case 59161:
@@ -4212,13 +4217,18 @@ void Aura::HandleAuraModIncreaseSpeed(bool /*apply*/, bool Real)
     m_target->UpdateSpeed(MOVE_RUN, true);
 }
 
-void Aura::HandleAuraModIncreaseMountedSpeed(bool /*apply*/, bool Real)
+void Aura::HandleAuraModIncreaseMountedSpeed(bool apply, bool Real)
 {
     // all applied/removed only at real aura add/remove
     if(!Real)
         return;
 
     m_target->UpdateSpeed(MOVE_RUN, true);
+
+    // Festive Holiday Mount
+    if (apply && GetSpellProto()->SpellIconID != 1794 && m_target->HasAura(62061))
+        // Reindeer Transformation
+        m_target->CastSpell(m_target, 25860, true, NULL, this);
 }
 
 void Aura::HandleAuraModIncreaseFlightSpeed(bool apply, bool Real)
@@ -4252,6 +4262,11 @@ void Aura::HandleAuraModIncreaseFlightSpeed(bool apply, bool Real)
         // Dragonmaw Illusion (overwrite mount model, mounted aura already applied)
         if( apply && m_target->HasAura(42016,0) && m_target->GetMountID())
             m_target->SetUInt32Value(UNIT_FIELD_MOUNTDISPLAYID,16314);
+
+        // Festive Holiday Mount
+        if (apply && GetSpellProto()->SpellIconID != 1794 && m_target->HasAura(62061))
+            // Reindeer Transformation
+            m_target->CastSpell(m_target, 25860, true, NULL, this);
     }
 
     m_target->UpdateSpeed(MOVE_FLIGHT, true);
