deabd0a06e96a3104a91515d8d7c3d0e5f7a355e
Ranger <Ranger@b8eaa9ba-51a4-de11-8768-001a646d8106>
2010-01-29 01:45:31 +0000

    - Возможность крита у ауры SPELL_AURA_PERIODIC_LEECH - два мемлика
    git-svn-id: svn://300murlocs.com/core/stable/code@629 b8eaa9ba-51a4-de11-8768-001a646d8106

--- src/game/CharacterHandler.cpp
+++ src/game/CharacterHandler.cpp
@@ -352,6 +352,7 @@ void WorldSession::HandleCharCreateOpcode( WorldPacket & recv_data )
                     {
                         data << (uint8)CHAR_CREATE_UNIQUE_CLASS_LIMIT;
                         SendPacket( &data );
+                        delete result2;
                         return;
                     }
                 }
@@ -406,6 +407,7 @@ void WorldSession::HandleCharCreateOpcode( WorldPacket & recv_data )
                         {
                             data << (uint8)CHAR_CREATE_UNIQUE_CLASS_LIMIT;
                             SendPacket( &data );
+                            delete result2;
                             return;
                         }
                     }
