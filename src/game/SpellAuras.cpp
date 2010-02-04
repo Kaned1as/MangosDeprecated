56eb01d7c4982d28e25e4a662f5f09dd39200c06
Ranger <Ranger@b8eaa9ba-51a4-de11-8768-001a646d8106>
2010-02-04 14:31:28 +0000

    revert r634 - дело в другом...
    WorldSession::LogUnexpectedOpcode 
    sLog.outError ->  sLog.outDebug чтобы убрать флуд с лога
    
    добавил спелл 13810 в исключения (против ERROR:Aura::TriggerSpell: Spell 13810 have 0 in EffectTriggered[1], not handled custom case?)
    
    git-svn-id: svn://300murlocs.com/core/stable/code@663 b8eaa9ba-51a4-de11-8768-001a646d8106

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2200,7 +2200,7 @@ void Aura::TriggerSpell()
         if (Unit* caster = GetCaster())
         {
             if(target->GetTypeId() != TYPEID_UNIT || !Script->EffectDummyCreature(caster, GetId(), GetEffIndex(), (Creature*)target))
-                if (GetId() != 39140)   //Ranger: ignoring spells
+                if (GetId() != 39140 && GetId() != 13810)   //Ranger: ignoring spells
                     sLog.outError("Aura::TriggerSpell: Spell %u have 0 in EffectTriggered[%d], not handled custom case?",GetId(),GetEffIndex());
         }
     }
