6a542788659c27da525f76661bca637317afb79c
Ranger <Ranger@b8eaa9ba-51a4-de11-8768-001a646d8106>
2010-02-17 13:50:34 +0000

    поправки
    git-svn-id: svn://300murlocs.com/core/stable/code@719 b8eaa9ba-51a4-de11-8768-001a646d8106

--- src/game/MovementHandler.cpp
+++ src/game/MovementHandler.cpp
@@ -179,14 +179,16 @@ bool WorldSession::Anti__ReportCheat(const char* Reason,float Speed,const char* 
         return true;
     }
 
+    /*
     //Ranger: Speed hack autoban in maps 0 & 1
     if ((Map == 0 || Map == 1) && Reason == "Speed hack" && Val2 > 600)
     {
         GetPlayer()->TeleportToHomebind();
         //sWorld.BanAccount(BAN_CHARACTER, Player, "1d", "Speed hack", "Anticheat");
     }
+    */
 
-    
+
     //Ranger: autoban system
     if (Map != 530 && Map !=571)                    //exception: map 530 & 571 (Outland and Northland)
     {
